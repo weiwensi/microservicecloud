c72dce627f4bf8cff13bd63f3c9dd85da2cbe572
wws11 <17637900215@163.com>
2019-05-09 10:36:22 +0800

    Eureka 集群搭建完毕

---
+++ microservicecloud-eureka-7002/src/main/java/com/gysoft/EurekaServer7002_App.java
@@ -0,0 +1,20 @@
+package com.gysoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description  注册中心启动类
+ * @Author DJZ-WWS
+ * @Date 2019/5/8 11:50
+ */
+@SpringBootApplication
+@EnableEurekaServer//EurekaServer服务器端启动类,接受其它微服务注册进来
+public class EurekaServer7002_App
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EurekaServer7002_App.class, args);
+    }
+}
